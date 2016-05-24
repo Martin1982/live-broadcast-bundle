2a156f0bbd2b97953467982b49af9fc3c24a3fe2
Martin de Keijzer <martin@spinninrecords.nl>
2016-05-24 18:21:35 +0200

    Add scheduler service

---
+++ Broadcaster/Scheduler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Martin1982\LiveBroadcastBundle\Broadcaster;
+
+class Scheduler
+{
+    public function checkRunningBroadcasts()
+    {
+
+    }
+
+    public function startBroadcast()
+    {
+
+    }
+
+    public function stopBroadcast()
+    {
+
+    }
+}
