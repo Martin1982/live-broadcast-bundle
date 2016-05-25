efa723d6fcc7022093500fd597c0132ed733f2d8
Martin de Keijzer <martin@spinninrecords.nl>
2016-05-25 11:36:10 +0200

    Scheduler prerequiste

--- Broadcaster/Scheduler.php
+++ Broadcaster/Scheduler.php
@@ -4,9 +4,14 @@ namespace Martin1982\LiveBroadcastBundle\Broadcaster;
 
 class Scheduler
 {
+    public function testPrerequisites()
+    {
+        var_dump($this->hasFFMpeg());
+    }
+
     public function checkRunningBroadcasts()
     {
-
+        $this->testPrerequisites();
     }
 
     public function startBroadcast()
@@ -17,5 +22,16 @@ class Scheduler
     public function stopBroadcast()
     {
 
+    }
+
+    protected function hasFFMpeg()
+    {
+        $cmdOutput = array();
+        exec('ffmpeg -version', $cmdOutput);
+        if (false !== strpos($cmdOutput[0], 'ffmpeg version')) {
+            return true;
+        }
+
+        return false;
     }
 }
