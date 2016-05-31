fc6d926cfbc8b91867e27b456ebcba48f2a34684
Martin de Keijzer <martin@spinninrecords.nl>
2016-05-31 17:13:45 +0200

    Add empty test

---
+++ Tests/Streams/Input/FileTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Martin1982\LiveBroadcastBundle\Tests\Streams\Input;
+
+use Martin1982\LiveBroadcastBundle\Streams\Input\File;
+
+class FileTest extends \PHPUnit_Framework_TestCase
+{
+    public function testNothing()
+    {
+        self::assertNull(null);
+    }
+}
