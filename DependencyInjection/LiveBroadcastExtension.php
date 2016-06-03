70007a42164cdff5d3a1fbf72d8aa8cfa1f15392
Martin de Keijzer <martin@spinninrecords.nl>
2016-06-03 17:25:16 +0200

    Fix Insights warnings

--- DependencyInjection/LiveBroadcastExtension.php
+++ DependencyInjection/LiveBroadcastExtension.php
@@ -2,7 +2,6 @@
 
 namespace Martin1982\LiveBroadcastBundle\DependencyInjection;
 
-use Symfony\Component\Config\Definition\Processor;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
@@ -19,9 +18,6 @@ class LiveBroadcastExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
