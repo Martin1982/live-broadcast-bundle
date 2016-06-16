ee2f8c37df27bbb6391d47580da029c3507481e1
Paul Radt <paul@spinninrecords.nl>
2016-06-16 17:08:21 +0200

    Add correct Facebook API call with AccessToken

--- Streams/Service/FacebookLiveService.php
+++ Streams/Service/FacebookLiveService.php
@@ -4,8 +4,9 @@ namespace Martin1982\LiveBroadcastBundle\Streams\Service;
 
 use Facebook\Exceptions\FacebookResponseException;
 use Facebook\Exceptions\FacebookSDKException;
-use Facebook\Facebook as FacebookAPI;
+use Facebook\Facebook as FacebookSDK;
 use Martin1982\LiveBroadcastBundle\Entity\LiveBroadcast;
+use Martin1982\LiveBroadcastBundle\Exception\LiveBroadcastException;
 use Martin1982\LiveBroadcastBundle\Streams\Output\Facebook as FacebookOutput;
 
 /**
@@ -17,10 +18,12 @@ class FacebookLiveService
     /**
      * @param LiveBroadcast  $liveBroadcast
      * @param FacebookOutput $facebookOutput
+     * @return null|string
+     * @throws LiveBroadcastException
      */
     public function createFacebookLiveVideo(LiveBroadcast $liveBroadcast, FacebookOutput $facebookOutput)
     {
-        $fbAPI = new FacebookAPI([
+        $fbSDK = new FacebookSDK([
             'app_id' => $facebookOutput->getApplicationId(),
             'app_secret' => $facebookOutput->getApplicationSecret(),
         ]);
@@ -29,16 +32,18 @@ class FacebookLiveService
             $params = array('title' => $liveBroadcast->getName(),
                             'description' => $liveBroadcast->getDescription());
 
-            $response = $fbAPI->post($facebookOutput->getEntityId().'/live_videos', $params);
+            $fbSDK->setDefaultAccessToken($facebookOutput->getAccessToken());
+            $response = $fbSDK->post($facebookOutput->getEntityId().'/live_videos', $params);
         } catch(FacebookResponseException $ex) {
-            echo 'Graph returned an error: ' . $ex->getMessage();
-            exit;
+            throw new LiveBroadcastException('Facebook exception: '.$ex->getMessage());
         } catch(FacebookSDKException $ex) {
-            // When validation fails or other local issues
-            echo 'Facebook SDK returned an error: ' . $ex->getMessage();
-            exit;
+            throw new LiveBroadcastException('Facebook SDK exception: '.$ex->getMessage());
         }
 
-        var_dump($response->getDecodedBody());
+        $body = $response->getDecodedBody();
+
+        if (array_key_exists('stream_url', $body)) {
+            return $body['stream_url'];
+        }
     }
 }
