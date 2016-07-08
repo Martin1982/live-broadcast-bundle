77e148bd5df93de38f649e453bc0b8211d63997d
Paul Radt <paul@spinninrecords.nl>
2016-07-08 15:37:16 +0200

    #33 Use long lived Facebook Access Tokens

---
+++ Controller/CRUDController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Martin1982\LiveBroadcastBundle\Controller;
+
+use Facebook\Authentication\AccessToken;
+use Martin1982\LiveBroadcastBundle\Streams\Service\FacebookLiveService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Sonata\AdminBundle\Controller\CRUDController as Controller;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Class CRUDController
+  */
+class CRUDController extends Controller
+{
+    /**
+     * @return JsonResponse
+     */
+    public function longLivedAccessTokenAction(Request $request)
+    {
+        /** @var FacebookLiveService $facebookService */
+        $facebookService = $this->get('live.broadcast.facebooklive.service');
+        $accessToken = $facebookService->getLongLivedAccessToken($request->get('userAccessToken', null));
+
+        if ($accessToken instanceof AccessToken) {
+            return new JsonResponse(array('accessToken' => $accessToken->getValue()));
+        }
+
+        return new JsonResponse(null, 500);
+    }
+}
