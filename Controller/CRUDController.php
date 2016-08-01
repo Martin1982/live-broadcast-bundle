ddb76374ab543fa934fe1dba510aa38b6afb098f
Martin de Keijzer <martin@spinninrecords.nl>
2016-08-01 16:55:33 +0200

    Add YouTube auth example

--- Controller/CRUDController.php
+++ Controller/CRUDController.php
@@ -3,6 +3,7 @@
 namespace Martin1982\LiveBroadcastBundle\Controller;
 
 use Facebook\Authentication\AccessToken;
+use Martin1982\LiveBroadcastBundle\Exception\LiveBroadcastException;
 use Martin1982\LiveBroadcastBundle\Service\FacebookLiveService;
 use Symfony\Component\HttpFoundation\Request;
 use Sonata\AdminBundle\Controller\CRUDController as Controller;
@@ -37,7 +38,27 @@ class CRUDController extends Controller
     public function youTubeAccessTokenAction(Request $request)
     {
         $youTubeService = $this->get('live.broadcast.youtubelive.service');
+        $session = $request->getSession();
 
+        $requestCode = $request->get('code');
+        $sessionCode = $session->get('code');
+
+        $requestState = $request->get('state');
+        $sessionState = $session->get('state');
+
+        if (isset($requestState)) {
+            if (strval($sessionState) !== strval($requestState)) {
+                throw new LiveBroadcastException('The session state did not match');
+            }
+
+            $youTubeService->authenticate($requestCode);
+            $session->set('token', $youTubeService->getAccessToken());
+            // Redir?
+        }
+
+        if (isset($sessionToken)) {
+            $youTubeService->setAccessToken($session->get('token'));
+        }
 
         return new JsonResponse(null, 500);
     }
